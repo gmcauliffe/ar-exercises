24b7e2f71aa9fc0d389ac9c2438dc33913b3f582
Guy <guy.mcauliffe@icloud.com>
2018-09-12 18:17:50 +0000

    Exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total company annual revenue = #{@total_revenue}"
+@avg_annual_revenue = Store.average(:annual_revenue)
+puts "Company average annual revenue = #{@avg_annual_revenue}"
